9f37a9ca03ae170cdb5b9fa8935994d81660498a
Joao <joaoheron@gmail.com>
2021-05-16 19:58:08 -0300

    Updated base bot constructor

--- eddy_bot/models/social_media_bot.py
+++ eddy_bot/models/social_media_bot.py
@@ -2,13 +2,8 @@ from eddy_bot.utils import get_resource, get_credentials, get_yaml
 
 class SocialMediaBot():
 
-    def __init__(self, credentials_path, tags_path, profiles_path, comments_path):
-        self.username, self.password = get_credentials(credentials_path)
-        self.tags = get_resource(tags_path)
-        self.profiles = get_resource(profiles_path)
-        self.comments = get_resource(comments_path)
-
-    def __init__(self, credentials_path, config_path):
+    def __init__(self, credentials_path, config_path, timeout=10):
+        self.timeout = timeout
         self.username, self.password = get_credentials(credentials_path)
         self.config = get_yaml(config_path)
         self.tags = self.config.get('tags')
