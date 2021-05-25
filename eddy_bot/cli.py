7962e78ec4d0f6ffe2175cb76146037ecb8669ab
Joao <joaoheron@gmail.com>
2021-05-24 22:56:17 -0300

    Updated command line interface

--- eddy_bot/cli.py
+++ eddy_bot/cli.py
@@ -17,7 +17,7 @@ def cli():
 @click.option('--config-path', '-c', default=vr.config_path, help='Configuration file path', required=False)
 def instagram(action, credentials_path, config_path):
     """
-        Build decision tree.
+        Build instagram bot.
     """
     try:
         bot = InstagramSeleniumBot(credentials_path=credentials_path, config_path=config_path)
@@ -27,16 +27,20 @@ def instagram(action, credentials_path, config_path):
         raise e
 
 @click.command()
-@click.option('--action', '-a', default='tweet', help='Action to be executed over this social media', required=True)
+@click.option('--tweet', '-t', default='This is a tweet', help='Message to be tweeted', required=False)
+@click.option('--follow', '-f', default='neymarjr', help='Profile to be followed', required=False)
 @click.option('--credentials-path', '-cr', default=vr.credentials_path, help='Credentials file path', required=False)
 @click.option('--config-path', '-c', default=vr.config_path, help='Configuration file path', required=False)
-def twitter(action, credentials_path, config_path):
+def twitter(tweet, profile, credentials_path, config_path):
     """
-        Build decision tree.
+        Build twitter bot.
     """
     try:
         bot = TwitterBot(credentials_path=credentials_path, config_path=config_path)
-        bot.tweet()
+        if tweet is not None:
+            bot.tweet(tweet)
+        if follow is not None:
+            bot.follow(profile)
     except Exception as e:
         raise e
 
