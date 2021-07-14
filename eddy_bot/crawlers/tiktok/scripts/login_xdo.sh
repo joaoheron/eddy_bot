4c24fc0593a0b51aad4cb95b62eecc02c98ab8c8
Joao <joao.heron@indicium.tech>
2021-07-14 00:21:05 -0300

    Finished tiktok login xdotool script

--- eddy_bot/crawlers/tiktok/scripts/login_xdo.sh
+++ eddy_bot/crawlers/tiktok/scripts/login_xdo.sh
@@ -1,57 +1,43 @@
 #!/bin/bash
-# login_xdo.sh - script logs into TikTok
-
+login_method="cellphone/email/google/facebook/twitter"
 url="https://www.tiktok.com/"
-email=""
-pwd=""
- 
-google-chrome $url &
- 
-# wait for the page to open, the set focus to it
-sleep 2
-# wmctrl -a "Tiktok - Chromium"
-# sleep 1 # allow time to get focus before sending keys
-# window_id = wmctrl -l | awk '/Google Chrome/ {print $1}'
+
+# Open browser and set focus
+google-chrome $url -incognito -new-window & 
+sleep 3
+
+# Maximize browser window
 window_id=$(wmctrl -l | awk '/Google Chrome/ {print strtonum($1)}')
-xdotool key alt+super+Up
-# xdotool windowsize $window_id 100% 100%
-echo "sleepy sleep"
-# xdotool windowminimize <window_id>
-xdotool mousemove 1000 1000
-xdotool click 2
+xdotool windowsize $window_id 100% 100%
+sleep 3
+
+# Click on Log In buttonlildobinho  Tiktoker22!
+xdotool mousemove 1400 100
+xdotool click 1
+sleep 3
+
+# Click on Log In method
+xdotool mousemove 900 400
+xdotool click 1
 sleep 2
-xdotool click 3
+
+# Click on Log In with username or email
+xdotool mousemove 1000 320
+xdotool click 1
 sleep 2
 
-xdotool mousemove 100 100 
-xdotool click 2
+# Click on username field
+xdotool mousemove 900 380
+xdotool click 1
+
+# Fill login information
+xte "str $TIKTOKER_USERNAME"
+xte "key Tab"
+xte "str $TIKTOKER_PASSWORD"
 sleep 2
 
-# xdotool click 3
-# sleep 2
+# Click on Log In button
+xdotool mousemove 900 550
+xdotool click 1
 
-# xdotool mousemove 20 500
-# xdotool click 2
-# sleep 2
-
-# xdotool click 3
-# sleep 2
-
-# xdotool mousemove 500 20
-# xdotool click 2
-# sleep 2
-
-# xdotool click 3
-
-# sleep 100
-
-# xte "str $email"
-# xte "str $pwd"
-# xte "key Tab"
-# xte "key Tab"
-# xte "key Tab"
-# xte "key Tab"
-# xte "key Tab"
-xte "key Return"
- 
-echo "Netflix Login Done..."
+sleep 10 # Solve catpcha
