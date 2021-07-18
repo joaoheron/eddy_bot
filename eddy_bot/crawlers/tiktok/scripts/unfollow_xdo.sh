aa1e1aa9b2313261518205667e7d6dd46245f969
Joao <joao.heron@indicium.tech>
2021-07-17 23:19:32 -0300

    Implemented unfollow xdo script

---
+++ eddy_bot/crawlers/tiktok/scripts/unfollow_xdo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# follow_xdo.sh - Script unfollows @user on TikTok
+url="https://www.tiktok.com/@$1"
+
+google-chrome $url -incognito & 
+sleep 3
+
+# Click on Follow button
+xdotool mousemove 1150 265
+xdotool click 1
+sleep 3
+
+xdotool key Ctrl+w
